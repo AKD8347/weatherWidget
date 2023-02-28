6aaf7835fcd768e51d26c6888074286ef116ef36
Alexandr Dmitrenko <a.akd4783@gmail.com>
2023-02-28 15:50:17 +0300

    get weather

--- src/Components/location/location.jsx
+++ src/Components/location/location.jsx
@@ -17,8 +17,7 @@ function LocationCity(props) {
         let hours = date.getHours();
         let minutes = "0" + date.getMinutes();
         let seconds = "0" + date.getSeconds();
-        let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-        return formattedTime;
+        return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
     }
 
 
